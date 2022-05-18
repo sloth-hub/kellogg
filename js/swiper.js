0303fbf8030fedf22bda8da22e0ae93853e578fe
sloth-hub <madamhh@gmail.com>
2022-05-18 20:30:23 +0900

    Feat: Swiper.js 슬라이더 추가

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 500,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    }
+});
