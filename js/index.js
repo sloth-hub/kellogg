65bba4c6e2640b73432f0dc8ef2e41a13debe159
sloth-hub <madamhh@gmail.com>
2022-06-10 17:14:12 +0900

    Feat: top 버튼과 top 버튼 애니메이션 추가

--- js/index.js
+++ js/index.js
@@ -3,6 +3,21 @@ $(".hamburger").on("click", () => {
     $("body").toggleClass("active");
 });
 
-$(".main-menu>.nav-item.sub").hover(()=> {
+$(".main-menu>.nav-item.sub").hover(() => {
     $("header").toggleClass("active");
+});
+
+$(window).on("scroll", () => {
+    let scrollY = $(document).scrollTop();
+    if (scrollY > 2450) {
+        $(".top").css({
+            "position": "absolute",
+            "transform": "scale(0)"
+        });
+    } else {
+        $(".top").css({
+            "position": "fixed",
+            "transform": "scale(1)"
+        });
+    }
 });
