f3883776158c604ff4df23972287e14e8518523b
sloth-hub <madamhh@gmail.com>
2022-06-10 16:28:11 +0900

    Feat: 헤더 내비게이션 css 수정

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ $(".hamburger").on("click", () => {
     $("nav").toggleClass("active");
     $("body").toggleClass("active");
 });
+
+$(".main-menu>.nav-item.sub").hover(()=> {
+    $("header").toggleClass("active");
+});
